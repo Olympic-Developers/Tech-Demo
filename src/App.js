d1aa9d4c40e80926fbe01e2122aad11eff4f6173
Peter Galvan <71851202+PeterJGalvan@users.noreply.github.com>
2021-12-02 15:04:35 -0700

    Just starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const poolData = {
+    UserPoolId: 'us-east-2_ZpPDf7ZeL',
+    ClientId: '3h8gft16acr1ho0n1fonsm78n'
+  };
+
+  const UserPool = new CognitoUserPool(poolData);
+
+  const onSubmit = event => {
+    event.preventDefault();
+
+    UserPool.signUp(email, password, [], null, ( err,data ) => {
+        if (err) console.error(err);
+        console.log(data);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>  
+
+        <input
+          value = {email}
+          onChange = {event => setEmail(event.target.value)}
+        />
+
+        <input
+          value = {password}
+          onChange = {event => setPassword(event.target.value)}
+        />
+
+        <button type='submit'> SignUp</button>
+      </form>
     </div>
-  );
-}
+  )
+
+};
 
 export default App;
